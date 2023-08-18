621b9619180413076f4e3d23c368c2f4497302d5
Femi <femzy14@gmail.com>
2023-08-18 13:24:56 +0100

    fixed navbar

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
@@ -8,12 +9,12 @@ const Navbar = () => {
           <div className=' flex items-center gap-10'>
 
             <div className='cursor-pointer'>
-              <img src="/assets/navicons/notify.svg" alt="notifications" className='' />
+              <Image src="/assets/navicons/notify.svg" width={30} height={30} alt="notifications" className='' />
             </div>
 
-            <div className='flex items-center cursor-pointer'>
+            <div className='flex items-center gap-2 cursor-pointer'>
               <div>
-                <img src="/assets/navicons/kalu.svg" alt=" profile picture" className='w-[80%]' />
+                <Image src="/assets/navicons/kalu.svg" width={40} height={40} alt=" profile picture" className='' />
               </div>
               <div>
                 <p className='font-semibold'>Kalu Abasiama</p>
@@ -22,7 +23,7 @@ const Navbar = () => {
             </div>
 
             <div className='flex items-end h-full -ml-5 cursor-pointer'>
-              <img src="/assets/navicons/arrowright.svg" alt="" />
+              <Image src="/assets/navicons/arrowright.svg" width={10} height={10} alt="" />
             </div>
           </div>
         </div>
