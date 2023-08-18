f1790174a76c928f709811bb85717fc4dbfe1fbd
Femi <femzy14@gmail.com>
2023-08-18 13:08:47 +0100

    fixed navbar

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -8,12 +8,12 @@ const Navbar = () => {
           <div className=' flex items-center gap-10'>
 
             <div className='cursor-pointer'>
-              <img src="/assets/navicons/notify.svg" alt="notifications" className=' bg-gray-100 rounded-full' />
+              <img src="/assets/navicons/notify.png" alt="notifications" className='' />
             </div>
 
             <div className='flex items-center cursor-pointer'>
               <div>
-                <img src="/assets/navicons/kalu.svg" alt=" profile picture" className='w-[80%] bg-gray-100 rounded-full' />
+                <img src="/assets/navicons/kalu.png" alt=" profile picture" className='w-[80%] bg-gray-100 rounded-full' />
               </div>
               <div>
                 <p className='font-semibold'>Kalu Abasiama</p>
@@ -22,7 +22,7 @@ const Navbar = () => {
             </div>
 
             <div className='flex items-end h-full -ml-5 cursor-pointer'>
-              <img src="/assets/navicons/arrowright.svg" alt="" />
+              <img src="/assets/navicons/arrowright.png" alt="" />
             </div>
           </div>
         </div>
